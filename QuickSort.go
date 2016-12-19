4a57ba0aff217115bac396d56e866d8f7be2242c
FrozenKP <csjh21010@gmail.com>
2016-12-20 01:14:08 +0800

    add other QuickSort

--- QuickSort.go
+++ QuickSort.go
@@ -51,3 +51,208 @@ func qsHandle(data []interface{}, left, right int, cmp func(i, j interface{}) bo
 	qsHandle(data, left, j-1, cmp)
 	qsHandle(data, j+1, right, cmp)
 }
+
+//QuickSort for float32, sorting from smallest to biggest.
+func QuickSortFloat32(data []float32) {
+	qsHandleFloat32(data, 0, len(data)-1)
+}
+
+func qsHandleFloat32(data []float32, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := data[left]
+	i, j := left+1, right
+
+	for true {
+		for i <= right {
+			if data[i] > pivot {
+				break
+			}
+			i++
+		}
+
+		for j > left {
+			if data[j] < pivot {
+				break
+			}
+			j = j - 1
+		}
+
+		if i > j {
+			break
+		} else {
+			data[i], data[j] = data[j], data[i]
+		}
+	}
+
+	data[left], data[j] = data[j], data[left]
+
+	qsHandleFloat32(data, left, j-1)
+	qsHandleFloat32(data, j+1, right)
+}
+
+//QuickSort for float64, sorting from smallest to biggest.
+func QuickSortFloat64(data []float64) {
+	qsHandleFloat64(data, 0, len(data)-1)
+}
+
+func qsHandleFloat64(data []float64, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := data[left]
+	i, j := left+1, right
+
+	for true {
+		for i <= right {
+			if data[i] > pivot {
+				break
+			}
+			i++
+		}
+
+		for j > left {
+			if data[j] < pivot {
+				break
+			}
+			j = j - 1
+		}
+
+		if i > j {
+			break
+		} else {
+			data[i], data[j] = data[j], data[i]
+		}
+	}
+
+	data[left], data[j] = data[j], data[left]
+
+	qsHandleFloat64(data, left, j-1)
+	qsHandleFloat64(data, j+1, right)
+}
+
+//QuickSort for int, sorting from smallest to biggest.
+func QuickSortInt(data []int) {
+	qsHandleInt(data, 0, len(data)-1)
+}
+
+func qsHandleInt(data []int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := data[left]
+	i, j := left+1, right
+
+	for true {
+		for i <= right {
+			if data[i] > pivot {
+				break
+			}
+			i++
+		}
+
+		for j > left {
+			if data[j] < pivot {
+				break
+			}
+			j = j - 1
+		}
+
+		if i > j {
+			break
+		} else {
+			data[i], data[j] = data[j], data[i]
+		}
+	}
+
+	data[left], data[j] = data[j], data[left]
+
+	qsHandleInt(data, left, j-1)
+	qsHandleInt(data, j+1, right)
+}
+
+//QuickSort for int32, sorting from smallest to biggest.
+func QuickSortInt32(data []int32) {
+	qsHandleInt32(data, 0, len(data)-1)
+}
+
+func qsHandleInt32(data []int32, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := data[left]
+	i, j := left+1, right
+
+	for true {
+		for i <= right {
+			if data[i] > pivot {
+				break
+			}
+			i++
+		}
+
+		for j > left {
+			if data[j] < pivot {
+				break
+			}
+			j = j - 1
+		}
+
+		if i > j {
+			break
+		} else {
+			data[i], data[j] = data[j], data[i]
+		}
+	}
+
+	data[left], data[j] = data[j], data[left]
+
+	qsHandleInt32(data, left, j-1)
+	qsHandleInt32(data, j+1, right)
+}
+
+//QuickSort for int64, sorting from smallest to biggest.
+func QuickSortInt64(data []int64) {
+	qsHandleInt64(data, 0, len(data)-1)
+}
+
+func qsHandleInt64(data []int64, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := data[left]
+	i, j := left+1, right
+
+	for true {
+		for i <= right {
+			if data[i] > pivot {
+				break
+			}
+			i++
+		}
+
+		for j > left {
+			if data[j] < pivot {
+				break
+			}
+			j = j - 1
+		}
+
+		if i > j {
+			break
+		} else {
+			data[i], data[j] = data[j], data[i]
+		}
+	}
+
+	data[left], data[j] = data[j], data[left]
+
+	qsHandleInt64(data, left, j-1)
+	qsHandleInt64(data, j+1, right)
+}
---
+++ example_QuickSortInt_test.go
@@ -0,0 +1,17 @@
+package sort_test
+
+import (
+  "github.com/go-Algorithm/sort"
+  "fmt"
+)
+
+func ExampleQuickSortInt(){
+  sample:=[]int{5,7,4,10,99,2,72,37}
+  fmt.Println(sample)
+
+  sort.QuickSortInt(sample)
+  fmt.Println(sample)
+  //Output:
+  //[5 7 4 10 99 2 72 37]
+  //[2 4 5 7 10 37 72 99]
+}
